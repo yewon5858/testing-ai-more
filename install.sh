656c2fe4d859e46f0bd67842f57f37b5ade9a13e
Ismael Barahona <isbaraho@ucm.es>
2024-02-04 12:34:24 +0100

    install modification

--- install.sh
+++ install.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-# Debemos de instalar nosotros python 3.8¿ pero ademas debemos tener en cuenta los diversos sistemas ?
-# Verificamos si Python 3.8 está instalado
-if command -v python3.8 &> /dev/null; then
-    echo "Python 3.8 ya está instalado."
-else
-    echo "Instalando Python 3.8..."
-    # Aquí se muestra un ejemplo para sistemas basados en Debian (como Ubuntu)
-    sudo apt-get update
-    sudo apt-get install -y python3.8
-fi
-
-# Instalar las dependencias del sistema (gcc,make y lib)
+# Instalar las dependencias del sistema
 sudo apt-get install -y gcc make libgmp3-dev
 
-# Instalar Requisitos
+# Instalar Requisitos // mirar si funciona con .
 pip install -r requirements.txt
 
 # Instalar solver MSat en pysmt 
 pysmt-install --msat
+
  #Mostramos un mensaje de finalizacion
  echo "Todos los elementos necesarios han sido instalados en su entorno"
