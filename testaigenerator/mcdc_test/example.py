076b1bd55b5d4c1c772d7c24d22a555c78112068
Gonz21 <goncontr@ucm.es>
2024-04-23 19:28:02 +0200

    changed command names

--- testaigenerator/mcdc_test/example.py
+++ testaigenerator/mcdc_test/example.py
@@ -5,7 +5,15 @@ from random import Random
 if __name__ == '__main__':
 
     reuse_h = LongestMayMerge
-    eq = '(a > 10) & (b < 9)'
+    a = int
+    b = int
+
+# La condición que se desea estudiar es eq.
+    eq = (a > 10) & (b < 9)
+    
+    
+    
+    
     rng = Random(100)
 
     result = solve(eq, reuse_h, rng)
