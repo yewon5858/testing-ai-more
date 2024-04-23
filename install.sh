a120f5753e530fff914f98cf7ba32c1d25e71705
Ismael Barahona <79473853+Isbaraho@users.noreply.github.com>
2024-04-23 23:46:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Instalar las dependencias del sistema
-sudo apt-get install -y gcc make libgmp3-dev
+sudo apt-get install -y gcc make  libgmp3-dev
 
 # Instalar Requisitos // mirar si funciona con .
 pip install -r requirements.txt
-
 # Instalar solver MSat en pysmt 
 pysmt-install --msat
-
+pysmt-install --check
  #Mostramos un mensaje de finalizacion
  echo "Todos los elementos necesarios han sido instalados en su entorno"
