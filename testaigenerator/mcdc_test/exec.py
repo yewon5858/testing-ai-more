5cd1951b94c4a482edf00a1b68c4f05bcda1df46
Ignacio Requeno <ppnacho85@gmail.com>
2024-04-27 18:06:16 +0200

    WIP

--- testaigenerator/mcdc_test/exec.py
+++ testaigenerator/mcdc_test/exec.py
@@ -1,8 +1,8 @@
 import sys
 from random import Random
 
-from mcdc_test_generator.mcdc_testcase.bdd_engine.path_search import LongestMayMerge, LongestPath, LongestBool, LongestBoolMay, BetterSize, RandomReuser
-from mcdc_testcase.generator import solve
+from mcdc_testcase.bdd_engine.path_search import LongestMayMerge, LongestPath, LongestBool, LongestBoolMay, BetterSize, RandomReuser
+from mcdc_testcase.generator.generator import solve
 
 if __name__ == '__main__':
 
