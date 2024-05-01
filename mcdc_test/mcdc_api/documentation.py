7f9b75916fc84bcbe918cbfc095fb62d1aa5584e
SoftwareStackLukas <Lukas-Sebastian.Hofmann@gmx.de>
2024-05-01 19:27:00 +0200

    Removing the mcdc_test

--- mcdc_test/mcdc_api/documentation.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from flask_restful import Resource
-
-class Documentation(Resource):
-    def get(self):
-        return jsonify({'message': 'Hello, to the MC/DC test generator!'}, 
-                    {'content': 'We will provide you here with the API specifications'},
-                    {'documentation': 'XY'})    
